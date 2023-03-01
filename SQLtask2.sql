ab624d030502395f674207a8c316ae5e98f893e8
Sanjaykumar-95 <84496961+Sanjaykumar-95@users.noreply.github.com>
2023-03-01 23:04:20 +0530

    Update SQLtask2.sql

--- SQLtask2.sql
+++ SQLtask2.sql
@@ -11,7 +11,7 @@ capacity int
 -- 3)Insert 10 details in the “Football Venue” table  -
 insert into football_venue values(1,"France",50030,100);
 insert into football_venue values(2,"Switzerland",50031,150);
-insert into football_venue values(3,"Russia",50032,120);
+insert into football_venue values(3,"Australia",50032,120);
 insert into football_venue values(4,"Mexico",50033,180);
 insert into football_venue values(5,"Sweden",50034,140);
 insert into football_venue values(6,"Italy",50035,170);
@@ -25,6 +25,6 @@ select count(venue_name)  from football_venue;
 
 -- 5)List all the venue names and capacities in the format of “Location” and “Volume”.
 select  venue_name as Location , capacity as Volume from football_venue
--- 6) Delete all the details where venue_name is equal to “Australia”
 
-delete from  football_venue where venue_name="Russia";
+-- 6) Delete all the details where venue_name is equal to “Australia”
+delete from  football_venue where venue_name="Australia";
